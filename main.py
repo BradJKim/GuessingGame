814b3513bddeafedfe0e06a884f7ba9f76f75015
BradJKim <bradjkim03@gmail.com>
2023-02-06 13:38:45 -0800

    Completed Game Functionality

--- main.py
+++ main.py
@@ -7,31 +7,46 @@ score =0
 keepPlaying = True
 
 
-input("Let's play the guessing game. I choose a number between 1-10 and you guess.")
+print("Let's play the guessing game. I choose a number between 1-10 and you guess.")
 
 while(keepPlaying):
     
     # Chooses number and asks user to guess the number until chosen number is picked
     # Will output if guess is higher or lower than chosen number
 
-    goal = random.random() * 10 + 1
-    guess = input("New number generated, guess the number: ")
+    goal = int(random.random() * 10 + 1)
+    guess = input("\nNew number generated, guess the number: ")
 
-    if(guess == goal):
+    while(True):
+
         tries+=1
-        score+=1
-        print("You guess the number correctly! You got the number %d, in %d. Total Score: %d", (goal, tries, score))
-    else:
-        if():
-            print("The number is higher")
+
+        try:
+            guess = int(guess)
+        except ValueError:
+            print("wtf, that's not a number?!?!")
+            guess = -1
+
+        if(guess == -1):
+            guess = input("Guess the number and actually input a number: ")
+        elif(guess == goal):
+            score+=1
+            print(f"\nYou guess the number correctly! You got the number {goal} in {tries} tries. Total Score: {score}")
+            tries =0
+            break
         else:
-            print("The number is lower")
+            if(guess > goal):
+                print("The number is lower")
+            else:
+                print("The number is higher")
+            guess = input("\nGuess the number: ")
+        
     
 
     # input validation for next round, checks if player put in corect input and wants to play another 
 
     while(True):
-        playAgain = input("Do you want to play again? (y/n): ")
+        playAgain = input("\nDo you want to play again? (y/n): ")
         if(playAgain == "n" or playAgain == "N"):
             keepPlaying = False
             break
@@ -41,4 +56,4 @@ while(keepPlaying):
         else:
             print("Invalid input, try again: ")
 
-print("Pass")
+print("Thanks for playing!\nFinal Score: %d\n"%(score))
